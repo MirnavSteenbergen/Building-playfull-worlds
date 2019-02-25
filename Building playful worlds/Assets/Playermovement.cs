3c3383b3b0e83444bf0f8a696da08b338ee2051d
MirnavSteenbergen <mirna.van.s@gmail.com>
2019-02-25 16:23:38 +0100

    hele project erin gegooid

---
+++ Building playful worlds/Assets/Playermovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    public float RotateSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        Vector3 Movevalues = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Movevalues *= MoveSpeed;
+        controller.Move(Movevalues * Time.deltaTime);
+    }
+}
