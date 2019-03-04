f0929c36337fc508a71b9cea7dc8ebe12ee0cfa4
MirnavSteenbergen <mirna.van.s@gmail.com>
2019-03-04 16:36:31 +0100

    weaponswitch added

---
+++ Building playful worlds/Assets/Weaponmanager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Weaponmanager : MonoBehaviour
+{
+    public GameObject[] weapons;
+    private int index;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SwitchWeapon(0);
+    }
+
+
+   
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            index++;
+
+            if (index > weapons.Length - 1)
+                index = 0;
+
+            SwitchWeapon(index);
+        }
+    }
+
+    private void SwitchWeapon(int newIndex)
+    {
+        
+        for (int i = 0; i < weapons.Length; i++)
+            weapons[i].gameObject.SetActive(false);
+
+        weapons[newIndex].gameObject.SetActive(true);
+
+    }
+}
