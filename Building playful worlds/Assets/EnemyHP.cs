c2a4856479ac18a44fcb874c8bdfc59f12d2c383
MirnavSteenbergen <mirna.van.s@gmail.com>
2019-03-04 18:00:05 +0100

    HP enemy added

---
+++ Building playful worlds/Assets/EnemyHP.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    public int HPmax = 10;
+    private int HPcurrent;
+    public int weapondamage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HPcurrent = HPmax;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "weapon")
+        {
+            HPcurrent -= weapondamage;
+            
+            if (HPcurrent <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
